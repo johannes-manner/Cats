9ccee7766a60f4c72ce9efbb44aa20aa850831a8
Robin Lichtenthaeler <robin.lichtenthaeler@web.de>
2020-06-01 18:21:16 +0200

    updated gradle wrapper and added possibility to stop service

--- src/main/java/de/uniba/dsg/jaxrs/JaxRsServer.java
+++ src/main/java/de/uniba/dsg/jaxrs/JaxRsServer.java
@@ -1,22 +1,27 @@
 package de.uniba.dsg.jaxrs;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.Properties;
 
 import javax.ws.rs.core.UriBuilder;
 
+import com.sun.net.httpserver.HttpServer;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class JaxRsServer {
     private static Properties properties = Configuration.loadProperties();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String serverUri = properties.getProperty("serverUri");
 
         URI baseUri = UriBuilder.fromUri(serverUri).build();
         ResourceConfig config = ResourceConfig.forApplicationClass(ExamplesApi.class);
-        JdkHttpServerFactory.createHttpServer(baseUri, config);
+        HttpServer server = JdkHttpServerFactory.createHttpServer(baseUri, config);
         System.out.println("Server ready to serve your JAX-RS requests...");
+        System.out.println("Press any key to exit...");
+        System.in.read();
+        server.stop(0);
     }
 }
